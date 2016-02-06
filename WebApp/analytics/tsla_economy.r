0f7a991025ad58a7b4c55b5306425a4cdbaa7f2c
Jesh <jskracht@gmail.com>
2016-02-06 15:29:43 -0700

    Update dates in R Files

--- WebApp/analytics/tsla_economy.r
+++ WebApp/analytics/tsla_economy.r
@@ -15,7 +15,7 @@ trainingPercent <- 0.80
 normalize <- TRUE
 frequency <- "daily"
 start <- "2011-01-01"
-end <- "2015-10-01"
+end <- "2016-02-01"
 futureDataPoints <- 12
  
 ################## FUNCTIONS ##################
