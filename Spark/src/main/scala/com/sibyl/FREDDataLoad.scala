0013d520576c2d582e17380ecbf83dda9ddf64ae
Jesh <jskracht@gmail.com>
2017-01-01 09:34:57 -0700

    Read in Date and Double objects while handling nulls using XML Access data from inside new dataframe

--- Spark/src/main/scala/com/sibyl/FREDDataLoad.scala
+++ Spark/src/main/scala/com/sibyl/FREDDataLoad.scala
@@ -3,55 +3,60 @@ package com.sibyl
 import java.io._
 import java.util.Date
 
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+
 import scala.io.Source
 import scalaj.http.{Http, HttpResponse}
-import org.apache.spark.sql.SparkSession
+import com.databricks.spark.xml
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.types.{StructField, _}
+
+import scala.collection.mutable
 
 /**
   * Created by Jesh on 11/27/16.
   */
 object FREDDataLoad {
-  case class Observation(date: Date, value: Double)
+  case class Observation(observation: Array[String])
 
   def main(args: Array[String]): Unit = {
     // Session Setup (http://spark.apache.org/docs/latest/submitting-applications.html#master-urls)
     val spark = SparkSession.builder.master("local").appName("Sibyl").getOrCreate()
 
     // Load Data
-    val seriesIds = Source.fromFile("data/seriesList").getLines()
+    val seriesIds = Source.fromFile("data/seriesList").getLines().toList
     val seriesCount = seriesIds.length
     var count = 1
+    var allSeries = loadSeriesFromAPI(seriesIds.take(1).head, spark)
     for (seriesId <- Source.fromFile("data/seriesList").getLines()) {
-      val series = loadSeriesFromAPI(seriesId, spark)
+      if (count > 1) {
+        val series = loadSeriesFromAPI(seriesId, spark)
+        allSeries = allSeries.union(series)
+      }
       println(count + " of " + seriesCount + " imported")
       count = count + 1
+      //allSeries.show()
     }
   }
 
-  def loadSeriesFromAPI(seriesId: String, spark: SparkSession): org.apache.spark.sql.DataFrame = {
-    val response: HttpResponse[String] = Http("https://api.stlouisfed.org/fred/series/observations").param("series_id", seriesId).param("realtime_start", "1930-01-01").param("api_key", "0ed28d55d3e9655415b8e31652c8a952").param("file_type", "json").asString
-    val file = new File("data/temp.json")
+  def loadSeriesFromAPI(seriesId: String, spark: SparkSession): DataFrame = {
+    val response: HttpResponse[String] = Http("https://api.stlouisfed.org/fred/series/observations").param("series_id", seriesId).param("realtime_start", "1930-01-01").param("api_key", "0ed28d55d3e9655415b8e31652c8a952").asString
+    val file = new File("data/temp.xml")
     val bw = new BufferedWriter(new FileWriter(file))
     bw.write(response.body)
     bw.close()
 
     val observations = StructType(Array(
-      StructField("date", TimestampType, true),
-      StructField("value", StringType, true)))
+      StructField("_date", DateType, true),
+      StructField("_value", DoubleType, true)))
     val schema = StructType(Array(
-      StructField("observations", ArrayType(observations, true), true)))
+      StructField("observation", ArrayType(observations, true), true)))
 
-    val series = spark.read.schema(schema).json("data/temp.json")
+    val series = spark.sqlContext.read.format("com.databricks.spark.xml").option("rowTag", "observations").option("nullValue", ".").schema(schema).load("data/temp.xml")
+    for (test <- series.select("observation._value").head.toSeq.head.asInstanceOf[mutable.WrappedArray[Double]].iterator)
+      {
+        println(test)
+      }
     series
-  }
-
-  def toDouble(value: String):Double = {
-    try {
-      return value.toDouble
-    } catch {
-      case e: NumberFormatException => None
-    }
-    null.asInstanceOf[Double]
   }
 }
