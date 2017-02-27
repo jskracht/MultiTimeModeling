f0c4fe1899d5a89042249a55244b28194e30c634
Jesh <jskracht@gmail.com>
2017-02-26 18:23:56 -0700

    Add Normalization of Data

--- Spark/src/main/scala/com/sibyl/FREDDataLoad.scala
+++ Spark/src/main/scala/com/sibyl/FREDDataLoad.scala
@@ -4,8 +4,11 @@ import java.sql.Timestamp
 import java.time.{LocalDateTime, ZoneId, ZonedDateTime}
 import java.util.Date
 
-import com.cloudera.sparkts.{BusinessDayFrequency, DateTimeIndex, TimeSeriesRDD}
+import com.cloudera.sparkts.{DateTimeIndex, DayFrequency, TimeSeriesRDD}
+import org.apache.spark.ml.linalg.{DenseVector, Vectors}
+import org.apache.spark.ml.feature.MinMaxScaler
 import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.sql.functions.udf
 
 import scala.io.Source
 import scala.util.parsing.json._
@@ -39,8 +42,8 @@ object FREDDataLoad extends App {
   def parseDoubleOrZero(value : String): Double = try { value.toDouble }  catch { case _ :Exception => null.asInstanceOf[Double] }
 
   var rows = scala.collection.mutable.ListBuffer[Row]()
-  val seriesIds = Source.fromFile("data/seriesList2").getLines().toList
-  for (series <- seriesIds) {
+  val seriesIDs = Source.fromFile("data/seriesList2").getLines().toList
+  for (series <- seriesIDs) {
     println("Fetching Series: " + series)
 
     val content = get("https://api.stlouisfed.org/fred/series/observations?series_id=%s&api_key=%s&file_type=json".
@@ -57,30 +60,48 @@ object FREDDataLoad extends App {
     }
   }
 
-  val rowRdd = spark.sparkContext.parallelize(rows)
+  //Create DataFrame
+  val rowRDD = spark.sparkContext.parallelize(rows)
   val fields = Seq(
     StructField("date", TimestampType, true),
     StructField("series", StringType, true),
-    StructField("value", DoubleType, true)
+    StructField("rawValue", DoubleType, true)
   )
   val schema = StructType(fields)
-  val timeSeriesData = spark.sqlContext.createDataFrame(rowRdd, schema)
-  timeSeriesData.show()
+  val rawTimeSeriesData = spark.sqlContext.createDataFrame(rowRDD, schema)
+
+  //Vectorize raw values to use Min/Max Scaler for normalization
+  val vectorize = udf((rawValue:Double) => Vectors.dense(Array(rawValue)))
+  val vectorizedTimeSeriesData = rawTimeSeriesData.withColumn("rawValue", vectorize(rawTimeSeriesData("rawValue")))
+  val scaler = new MinMaxScaler()
+    .setInputCol("rawValue")
+    .setOutputCol("value")
+    .setMax(1)
+    .setMin(-1)
+
+  val normalizedTimeSeriesData = scaler.fit(vectorizedTimeSeriesData).transform(vectorizedTimeSeriesData)
+
+  //Convert vectorized values back to doubles
+  val devectorize = udf{ value:DenseVector => value(0) }
+  val devectorizedNormalizedTimeSeriesData = normalizedTimeSeriesData.withColumn("value", devectorize(normalizedTimeSeriesData("value")))
+  devectorizedNormalizedTimeSeriesData.show()
 
   //Create DateTimeIndex
   val zone = ZoneId.systemDefault()
   val dateTimeIndex = DateTimeIndex.uniformFromInterval(
     ZonedDateTime.of(LocalDateTime.parse("1960-01-01T00:00:00"), zone),
-    ZonedDateTime.of(LocalDateTime.parse("2017-01-01T00:00:00"), zone),
-    new BusinessDayFrequency(1))
+    ZonedDateTime.of(LocalDateTime.parse("2017-01-01T00:00:00"), zone), new DayFrequency(1))
 
   //Put data into TimeSeriesRDD
-  val timeSeriesRdd = TimeSeriesRDD.timeSeriesRDDFromObservations(dateTimeIndex, timeSeriesData,
+  val timeSeriesRDD = TimeSeriesRDD.timeSeriesRDDFromObservations(dateTimeIndex, devectorizedNormalizedTimeSeriesData,
     "date", "series", "value")
 
   //Cache in memory
-  timeSeriesRdd.cache()
+  timeSeriesRDD.cache()
 
   //Fill in null values using linear interpolation
-  val filled = timeSeriesRdd.fill("linear")
+  val filledTimeSeriesRDD = timeSeriesRDD.fill("linear")
+  val sliced = filledTimeSeriesRDD.slice(ZonedDateTime.of(LocalDateTime.parse("1996-01-01T00:00:00"), zone), ZonedDateTime.of(LocalDateTime.parse("2017-01-01T00:00:00"), zone))
+
+  println(sliced.collect().head)
 }
