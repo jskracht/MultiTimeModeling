386b9d3e20bf42c9d35585b3877c89bb3ae78382
Jesh <jskracht@gmail.com>
2021-01-26 12:41:43 -0700

    Update for Latest Data and Runtime

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Sibyl',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=['quandl', 'matplotlib', 'tensorflow', 'pandas', 'pycausalimpact', 'preprocessing', 'sklearn']
 )
