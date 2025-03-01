7d4adf6825c676b3ccc622c7569796be667bf627
jskracht <jeshuak@me.com>
2025-02-28 20:42:58 -0700

    Switch to FRED API and Improve Prediction

--- LSTM.py
+++ LSTM.py
@@ -1,22 +1,79 @@
-import quandl
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from sklearn import preprocessing
 from datetime import datetime
-quandl.ApiConfig.api_key = "ti-yR6gd8be4x4yswvy4"
+from fredapi import Fred
+import time
+import os
+fred = Fred(api_key="a8b743935c751548fe996ee153f3fbde")
+
+# Constants
+DATA_CACHE_FILE = 'fred_data_cache.csv'
+RATE_LIMIT_DELAY = 0.5  # Half second delay between requests
+
+# Function to fetch data for a single series with rate limiting
+def fetch_fred_series(series_id, start_date, end_date):
+    try:
+        # Add delay for rate limiting
+        time.sleep(RATE_LIMIT_DELAY)
+        
+        # First try with monthly frequency
+        try:
+            data = fred.get_series(series_id, start_date=start_date, end_date=end_date, frequency='m')
+        except Exception as freq_error:
+            # If monthly frequency fails, try without specifying frequency
+            data = fred.get_series(series_id, start_date=start_date, end_date=end_date)
+            # Resample to monthly frequency using month end ('ME')
+            data = data.asfreq('ME', method='ffill')  # Forward fill missing values
+            # If there are still NaN values, interpolate them
+            data = data.interpolate(method='time')
+        
+        return pd.Series(data, name=series_id)
+    except Exception as e:
+        print(f"Error fetching {series_id}: {str(e)}")
+        return pd.Series(name=series_id)
+
+def load_or_fetch_data(features, start_date, end_date):
+    """Load data from cache if available, otherwise fetch from FRED"""
+    if os.path.exists(DATA_CACHE_FILE):
+        print("Loading data from local cache...")
+        cached_data = pd.read_csv(DATA_CACHE_FILE, index_col=0, parse_dates=True)
+        
+        # Check if we need to update the cache
+        if cached_data.index[-1].strftime('%Y-%m-%d') >= end_date:
+            print("Cache is up to date!")
+            return cached_data
+        else:
+            print("Cache exists but needs updating...")
+    
+    print("Fetching data from FRED (this may take a while due to rate limiting)...")
+    all_series = []
+    total_features = len(features)
+    
+    for i, series_id in enumerate(features, 1):
+        print(f"Fetching {series_id} ({i}/{total_features})...")
+        series = fetch_fred_series(series_id, start_date, end_date)
+        all_series.append(series)
+    
+    dataframe = pd.concat(all_series, axis=1)
+    
+    # Save to cache
+    print("Saving data to cache...")
+    dataframe.to_csv(DATA_CACHE_FILE)
+    
+    return dataframe
 
 # Pull Raw Data
-current_month = datetime.now().strftime('%Y') + '-' + datetime.now().strftime('%m') + '-01'
-# List from https://fredaccount.stlouisfed.org/public/datalist/?st=&pageID=1 make sure `RECPROUSM156N` is first in list and not duplicated
-features = ["FRED/RECPROUSM156N", "FRED/ACOILBRENTEU","FRED/ACOILWTICO","FRED/AHETPI","FRED/AISRSA","FRED/AUINSA","FRED/AWHAETP","FRED/BAA10Y","FRED/BUSINV","FRED/CANTOT","FRED/CBI","FRED/CDSP","FRED/CES0500000003","FRED/CEU0500000002","FRED/CEU0500000003","FRED/CEU0500000008","FRED/CHNTOT","FRED/CIVPART","FRED/CNP16OV","FRED/COMPNFB","FRED/COMPRNFB","FRED/COMREPUSQ159N","FRED/CPIAUCNS","FRED/CPIAUCSL","FRED/DCOILBRENTEU","FRED/DCOILWTICO","FRED/DDDM01USA156NWDB","FRED/DED1","FRED/DED3","FRED/DED6","FRED/DEXBZUS","FRED/DEXCAUS","FRED/DEXCHUS","FRED/DEXJPUS","FRED/DEXKOUS","FRED/DEXMXUS","FRED/DEXNOUS","FRED/DEXSDUS","FRED/DEXSFUS","FRED/DEXSIUS","FRED/DEXSZUS","FRED/DEXUSAL","FRED/DEXUSEU","FRED/DEXUSNZ","FRED/DEXUSUK","FRED/DGORDER","FRED/DGS10","FRED/DSPIC96","FRED/DSWP1","FRED/DSWP10","FRED/DSWP2","FRED/DSWP3","FRED/DSWP30","FRED/DSWP4","FRED/DSWP5","FRED/DSWP7","FRED/DTWEXB","FRED/DTWEXM","FRED/ECIWAG","FRED/ECOMNSA","FRED/ECOMSA","FRED/EECTOT","FRED/EMRATIO","FRED/ETOTALUSQ176N","FRED/EVACANTUSQ176N","FRED/FEDFUNDS","FRED/FRNTOT","FRED/FYFSGDA188S","FRED/GASREGCOVM","FRED/GASREGCOVW","FRED/GASREGM","FRED/GASREGW","FRED/GCT1501US","FRED/GCT1502US","FRED/GCT1503US","FRED/GERTOT","FRED/GOLDAMGBD228NLBM","FRED/GOLDPMGBD228NLBM","FRED/HCOMPBS","FRED/HDTGPDUSQ163N","FRED/HOABS","FRED/HOANBS","FRED/HOUST","FRED/HPIPONM226N","FRED/HPIPONM226S","FRED/IC4WSA","FRED/INDPRO","FRED/INTDSRUSM193N","FRED/IPBUSEQ","FRED/IPDBS","FRED/IPMAN","FRED/IPMAT","FRED/IPMINE","FRED/IR","FRED/IR10010","FRED/IREXPET","FRED/ISRATIO","FRED/JCXFE","FRED/JPNTOT","FRED/JTS1000HIL","FRED/JTS1000HIR","FRED/JTSHIL","FRED/JTSHIR","FRED/JTSJOL","FRED/JTSJOR","FRED/JTSLDL","FRED/JTSLDR","FRED/JTSQUL","FRED/JTSQUR","FRED/JTSTSL","FRED/JTSTSR","FRED/JTU1000HIL","FRED/JTU1000HIR","FRED/JTUHIL","FRED/JTUHIR","FRED/JTUJOL","FRED/JTUJOR","FRED/JTULDL","FRED/JTULDR","FRED/JTUQUL","FRED/JTUQUR","FRED/JTUTSL","FRED/JTUTSR","FRED/LNS12032194","FRED/LNS12032196","FRED/LNS14027660","FRED/LNS15000000","FRED/LNU05026642","FRED/M12MTVUSM227NFWA","FRED/M2V","FRED/MCOILBRENTEU","FRED/MCOILWTICO","FRED/MCUMFN","FRED/MEHOINUSA646N","FRED/MEHOINUSA672N","FRED/MFGOPH","FRED/MFGPROD","FRED/MNFCTRIRNSA","FRED/MNFCTRIRSA","FRED/MNFCTRMPCSMNSA","FRED/MNFCTRMPCSMSA","FRED/MNFCTRSMNSA","FRED/MNFCTRSMSA","FRED/MYAGM2USM052N","FRED/MYAGM2USM052S","FRED/NAPM","FRED/NAPMBI","FRED/NAPMCI","FRED/NAPMEI","FRED/NAPMEXI","FRED/NAPMII","FRED/NAPMIMP","FRED/NAPMNOI","FRED/NAPMPI","FRED/NAPMPRI","FRED/NAPMSDI","FRED/NILFWJN","FRED/NILFWJNN","FRED/NMFBAI","FRED/NMFBI","FRED/NMFEI","FRED/NMFEXI","FRED/NMFIMI","FRED/NMFINI","FRED/NMFINSI","FRED/NMFNOI","FRED/NMFPI","FRED/NMFSDI","FRED/NROU","FRED/NROUST","FRED/OPHMFG","FRED/OPHNFB","FRED/OPHPBS","FRED/OUTBS","FRED/OUTMS","FRED/OUTNFB","FRED/PAYEMS","FRED/PAYNSA","FRED/PCE","FRED/PCEPI","FRED/PCEPILFE","FRED/PCETRIM12M159SFRBDAL","FRED/PCETRIM1M158SFRBDAL","FRED/PNRESCON","FRED/PNRESCONS","FRED/POP","FRED/POPTHM","FRED/PPIACO","FRED/PRRESCON","FRED/PRRESCONS","FRED/PRS30006013", "FRED/PRS30006023","FRED/PRS84006013","FRED/PRS84006023","FRED/PRS84006163","FRED/PRS84006173","FRED/PRS85006023","FRED/PRS85006163","FRED/PRS85006173","FRED/RCPHBS","FRED/RETAILIMSA","FRED/RETAILIRSA","FRED/RETAILMPCSMNSA","FRED/RETAILMPCSMSA","FRED/RETAILSMNSA","FRED/RETAILSMSA","FRED/RHORUSQ156N","FRED/RIFLPCFANNM","FRED/RPI","FRED/RRSFS","FRED/RSAFS","FRED/RSAFSNA","FRED/RSAHORUSQ156S","FRED/RSEAS","FRED/RSFSXMV","FRED/RSNSR","FRED/RSXFS","FRED/T10Y2Y","FRED/T10Y3M","FRED/T10YFF","FRED/T10YIEM","FRED/T5YIEM","FRED/T5YIFR","FRED/TB3SMFFM","FRED/TCU","FRED/TDSP","FRED/TEDRATE","FRED/TLCOMCON","FRED/TLCOMCONS","FRED/TLNRESCON","FRED/TLNRESCONS","FRED/TLPBLCON","FRED/TLPBLCONS","FRED/TLPRVCON","FRED/TLPRVCONS","FRED/TLRESCON","FRED/TLRESCONS","FRED/TOTBUSIMNSA","FRED/TOTBUSIRNSA","FRED/TOTBUSMPCIMNSA","FRED/TOTBUSMPCIMSA","FRED/TOTBUSMPCSMNSA","FRED/TOTBUSMPCSMSA","FRED/TOTBUSSMNSA","FRED/TOTBUSSMSA","FRED/TOTDTEUSQ163N","FRED/TRFVOLUSM227NFWA","FRED/TTLCON","FRED/TTLCONS","FRED/U4RATE","FRED/U4RATENSA","FRED/U6RATE","FRED/U6RATENSA","FRED/UEMPMED","FRED/UKTOT","FRED/ULCBS","FRED/ULCMFG","FRED/ULCNFB","FRED/UNRATE","FRED/USAGDPDEFAISMEI","FRED/USAGDPDEFQISMEI","FRED/USAGFCFADSMEI","FRED/USAGFCFQDSMEI","FRED/USAGFCFQDSNAQ","FRED/USARECDM","FRED/USARGDPC","FRED/USASACRAISMEI","FRED/USASACRMISMEI","FRED/USASACRQISMEI","FRED/USPRIV","FRED/USRECD","FRED/USRECDM","FRED/USSLIND","FRED/USSTHPI","FRED/WCOILBRENTEU","FRED/WCOILWTICO","FRED/WHLSLRIRNSA","FRED/WHLSLRIRSA"]
-dataframe = quandl.get(features, start_date="1970-01-01", end_date=current_month, collapse="monthly")
+current_month = datetime.now().strftime('%Y-%m-%d')
+start_date = "1970-01-01"
 
-# Save to Machine For Quick Reuse
-# pd.to_pickle(dataframe, 'data/dataframe.dat')
+# List of FRED series IDs (removed 'FRED/' prefix from original list)
+features = ["RECPROUSM156N", "ACOILBRENTEU","ACOILWTICO","AHETPI","AISRSA","AUINSA","AWHAETP","BAA10Y","BUSINV","CANTOT","CBI","CDSP","CES0500000003","CEU0500000002","CEU0500000003","CEU0500000008","CHNTOT","CIVPART","CNP16OV","COMPNFB","COMPRNFB","COMREPUSQ159N","CPIAUCNS","CPIAUCSL","DCOILBRENTEU","DCOILWTICO","DDDM01USA156NWDB","DED1","DED3","DED6","DEXBZUS","DEXCAUS","DEXCHUS","DEXJPUS","DEXKOUS","DEXMXUS","DEXNOUS","DEXSDUS","DEXSFUS","DEXSIUS","DEXSZUS","DEXUSAL","DEXUSEU","DEXUSNZ","DEXUSUK","DGORDER","DGS10","DSPIC96","DSWP1","DSWP10","DSWP2","DSWP3","DSWP30","DSWP4","DSWP5","DSWP7","DTWEXB","DTWEXM","ECIWAG","ECOMNSA","ECOMSA","EECTOT","EMRATIO","ETOTALUSQ176N","EVACANTUSQ176N","FEDFUNDS","FRNTOT","FYFSGDA188S","GASREGCOVM","GASREGCOVW","GASREGM","GASREGW","GCT1501US","GCT1502US","GCT1503US","GERTOT","GOLDAMGBD228NLBM","GOLDPMGBD228NLBM","HCOMPBS","HDTGPDUSQ163N","HOABS","HOANBS","HOUST","HPIPONM226N","HPIPONM226S","IC4WSA","INDPRO","INTDSRUSM193N","IPBUSEQ","IPDBS","IPMAN","IPMAT","IPMINE","IR","IR10010","IREXPET","ISRATIO","JCXFE","JPNTOT","JTS1000HIL","JTS1000HIR","JTSHIL","JTSHIR","JTSJOL","JTSJOR","JTSLDL","JTSLDR","JTSQUL","JTSQUR","JTSTSL","JTSTSR","JTU1000HIL","JTU1000HIR","JTUHIL","JTUHIR","JTUJOL","JTUJOR","JTULDL","JTULDR","JTUQUL","JTUQUR","JTUTSL","JTUTSR","LNS12032194","LNS12032196","LNS14027660","LNS15000000","LNU05026642","M12MTVUSM227NFWA","M2V","MCOILBRENTEU","MCOILWTICO","MCUMFN","MEHOINUSA646N","MEHOINUSA672N","MFGOPH","MFGPROD","MNFCTRIRNSA","MNFCTRIRSA","MNFCTRMPCSMNSA","MNFCTRMPCSMSA","MNFCTRSMNSA","MNFCTRSMSA","MYAGM2USM052N","MYAGM2USM052S","NAPM","NAPMBI","NAPMCI","NAPMEI","NAPMEXI","NAPMII","NAPMIMP","NAPMNOI","NAPMPI","NAPMPRI","NAPMSDI","NILFWJN","NILFWJNN","NMFBAI","NMFBI","NMFEI","NMFEXI","NMFIMI","NMFINI","NMFINSI","NMFNOI","NMFPI","NMFSDI","NROU","NROUST","OPHMFG","OPHNFB","OPHPBS","OUTBS","OUTMS","OUTNFB","PAYEMS","PAYNSA","PCE","PCEPI","PCEPILFE","PCETRIM12M159SFRBDAL","PCETRIM1M158SFRBDAL","PNRESCON","PNRESCONS","POP","POPTHM","PPIACO","PRRESCON","PRRESCONS","PRS30006013", "PRS30006023","PRS84006013","PRS84006023","PRS84006163","PRS84006173","PRS85006023","PRS85006163","PRS85006173","RCPHBS","RETAILIMSA","RETAILIRSA","RETAILMPCSMNSA","RETAILMPCSMSA","RETAILSMNSA","RETAILSMSA","RHORUSQ156N","RIFLPCFANNM","RPI","RRSFS","RSAFS","RSAFSNA","RSAHORUSQ156S","RSEAS","RSFSXMV","RSNSR","RSXFS","T10Y2Y","T10Y3M","T10YFF","T10YIEM","T5YIEM","T5YIFR","TB3SMFFM","TCU","TDSP","TEDRATE","TLCOMCON","TLCOMCONS","TLNRESCON","TLNRESCONS","TLPBLCON","TLPBLCONS","TLPRVCON","TLPRVCONS","TLRESCON","TLRESCONS","TOTBUSIMNSA","TOTBUSIRNSA","TOTBUSMPCIMNSA","TOTBUSMPCIMSA","TOTBUSMPCSMNSA","TOTBUSMPCSMSA","TOTBUSSMNSA","TOTBUSSMSA","TOTDTEUSQ163N","TRFVOLUSM227NFWA","TTLCON","TTLCONS","U4RATE","U4RATENSA","U6RATE","U6RATENSA","UEMPMED","UKTOT","ULCBS","ULCMFG","ULCNFB","UNRATE","USAGDPDEFAISMEI","USAGDPDEFQISMEI","USAGFCFADSMEI","USAGFCFQDSMEI","USAGFCFQDSNAQ","USARECDM","USARGDPC","USASACRAISMEI","USASACRMISMEI","USASACRQISMEI","USPRIV","USRECD","USRECDM","USSLIND","USSTHPI","WCOILBRENTEU","WCOILWTICO","WHLSLRIRNSA","WHLSLRIRSA"]
 
-# Load Raw Data From Local Drive
-# dataframe = pd.read_pickle('data/dataframe.dat')
+# Load or fetch data
+dataframe = load_or_fetch_data(features, start_date, current_month)
 
 # Interpolate Data
 dataframe = dataframe.interpolate(method='time', limit_direction='both')
@@ -53,14 +110,57 @@ multi_step_model.fit(train_X, train_Y, epochs=30, batch_size=12, validation_data
 
 # Make Test Prediction
 prediction_Y = multi_step_model.predict(test_X)
+plt.figure(figsize=(12, 6))
 plt.plot(test_Y, label='actual')
 plt.plot(prediction_Y, label='predicted')
+plt.title('Test Set Predictions vs Actual Values')
 plt.legend()
 plt.show()
 
-# Make Future Prediction (Need to Use "Windowing" to Prepare Data First)
-all_X = all_X.reshape(all_X.shape[0], 1, all_X.shape[1])
-prediction_Y = multi_step_model.predict(all_X)
-plt.plot(prediction_Y, label='predicted')
+def make_future_forecast(model, last_known_values, n_future_steps):
+    """
+    Make future forecasts using the trained model
+    
+    Parameters:
+    model: trained LSTM model
+    last_known_values: the last known values of all features (normalized)
+    n_future_steps: number of future steps to predict
+    
+    Returns:
+    Array of predicted values
+    """
+    future_predictions = []
+    current_input = last_known_values.reshape(1, 1, -1)  # Reshape to match model input shape
+    
+    for _ in range(n_future_steps):
+        # Get the prediction for the next step
+        next_pred = model.predict(current_input)
+        future_predictions.append(next_pred[0, 0])
+        
+        # For demonstration, we'll use a simple approach where we keep the same feature values
+        # In a real application, you might want to update the features based on your domain knowledge
+        current_input = current_input  # Keep using the same features for next prediction
+    
+    return np.array(future_predictions)
+
+# Get the last known values (all features except the target)
+last_known_values = all_X[-1]  # Last row of features
+
+# Make future predictions for next 12 months
+n_future_months = 12
+future_predictions = make_future_forecast(multi_step_model, last_known_values, n_future_months)
+
+# Plot historical data and future predictions
+plt.figure(figsize=(15, 7))
+plt.plot(range(len(all_Y)), all_Y, label='Historical Data', color='blue')
+plt.plot(range(len(all_Y), len(all_Y) + n_future_months), future_predictions, 
+         label='Future Forecast', color='red', linestyle='--')
+plt.axvline(x=len(all_Y)-1, color='gray', linestyle='--', alpha=0.5)
+plt.title('Historical Data and Future Forecast')
 plt.legend()
 plt.show()
+
+# Print the future predictions
+print("\nFuture predictions for the next {} months:".format(n_future_months))
+for i, pred in enumerate(future_predictions, 1):
+    print(f"Month {i}: {pred:.4f}")
