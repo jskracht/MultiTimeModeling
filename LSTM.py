b5d71ba4eb5db65ccc0eaf7e434aee23d4124936
jskracht <jeshuak@me.com>
2025-02-28 21:30:46 -0700

    Avoid overfitting

--- LSTM.py
+++ LSTM.py
@@ -169,10 +169,14 @@ test_X = test_X.reshape(test_X.shape[0], 1, test_X.shape[1])
 # Build Model
 multi_step_model = tf.keras.models.Sequential([
     tf.keras.layers.Input(shape=(train_X.shape[1], train_X.shape[2])),
-    tf.keras.layers.LSTM(20),
-    tf.keras.layers.Dense(1)
+    tf.keras.layers.LSTM(16, return_sequences=False),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(1, 
+                         activation='sigmoid',
+                         kernel_regularizer=tf.keras.regularizers.l2(0.01))
 ])
-multi_step_model.compile(loss='mae', optimizer='adam')
+multi_step_model.compile(loss='binary_crossentropy', 
+                        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001))
 
 # Train Model
 multi_step_model.fit(train_X, train_Y, epochs=30, batch_size=12, validation_data=(test_X, test_Y), verbose=2, shuffle=False)
@@ -211,7 +215,7 @@ n_future_months = 12
 future_predictions = make_future_forecast(multi_step_model, last_known_values, n_future_months)
 
 # Create future dates for plotting
-last_date = dataframe.index[-1]
+last_date = pd.to_datetime(dataframe.index[-1])  # Ensure last_date is datetime
 future_dates = pd.date_range(start=last_date + pd.DateOffset(months=1), periods=n_future_months, freq='M')
 
 # Plot historical data and future predictions
